d19302841d00929683f4c1d3d8135152e125c9b0
Eric Marcy <ericmarcypro@gmail.com>
2022-09-11 22:07:51 -0400

    took a shot at tracking the number of questions, added to/revised index.HTML, added highscores.html

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,32 @@
-const button = document.getElementsByTagName('button')[0];
+const startBtn = document.getElementsByTagName('button')[0];
 let time = document.getElementById('time');
+let list = document.getElementsByTagName('ol')[0];
+let question = document.getElementsByTagName('h2')[0];
+let listItem = document.getElementsByTagName('li');
+
 let seconds = 75;
+
+listItem.style.display = "none";
+
+function qTracker(){
+    let clicks = 0;
+    if (startBtn.addEventListener('click')) clicks++;
+    for (x = 0; x < listItem.length; x++) {
+        if (listItem[x].addEventListener('click')) clicks++;
+    }
+
+}
 
 function start() {
     const counter = setInterval(countdown, 1000);
+    startBtn.style.display = "none";
 
     function countdown() {
         seconds--;
         time.innerText = seconds;
         if (seconds === 0) clearInterval(counter);
     }
+
 }
 
-button.addEventListener('click', start);
+startBtn.addEventListener('click', start);
